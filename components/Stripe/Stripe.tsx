773a3a756045e1047bf7f06201a4f8b44195e47d
ifraaa <ifraejaz07@gmail.com>
2024-08-11 00:28:28 +0500

    stripe fix

--- components/Stripe/Stripe.tsx
+++ components/Stripe/Stripe.tsx
@@ -1,13 +1,11 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 const StripePayment = () => {
   return (
     <div>
-      <Link href={'https://buy.stripe.com/test_00g7tBfqg7dM2Q0cMN'}>
-        Pay with Stripe
-      </Link>
-    </div>  
-  )
-}
+      <a>Pay with Stripe</a>
+    </div>
+  );
+};
 
-export default StripePayment
+export default StripePayment;
